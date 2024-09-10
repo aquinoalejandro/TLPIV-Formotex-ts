51ee9427934fc7c7ede97dd185bc3e61ba61bd12
aquinoalejandro <aquinoalejandro666@gmail.com>
2024-09-10 18:43:49 -0300

    feat: Intento de login

---
+++ client/src/pages/login.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+export const Login = () => {
+    return (
+        <div>
+            <h1>login</h1>
+                <form action="" method="post">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username"/>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password"/>
+                    <input type="submit" value="login"/>
+            </form>
+        </div>
+    );
+};
